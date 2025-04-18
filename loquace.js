98fd70bf27e4a4bdb645c08329313ce6d1971f0b
Loïc Cattani <loic@cattani.co>
2025-04-18 12:47:19 +0200

    Included deepMerge into loquace for a dependency-free plugin

--- loquace.js
+++ loquace.js
@@ -1,7 +1,5 @@
 // loquace.js: A Visual Novel and Interactive Fiction dialog system for KAPLAY
 
-import { deepMerge } from '../utils';
-
 // This plugin allows you to create visual novel dialogues in your games.
 // Designed for developers familiar with Ren'Py or Monogatari
 // Usable both as a KAPLAY plugin or as an ES6 module
@@ -567,4 +565,66 @@ function vn(string, options = {}) {
     }
 
     return textBoxObj; // Allow for further manipulation and/or custom tweening
+}
+
+// From https://gomakethings.com/how-to-deep-merge-arrays-and-objects-with-javascript/
+/*!
+ * Deep merge two or more objects or arrays.
+ * (c) 2023 Chris Ferdinandi, MIT License, https://gomakethings.com
+ * @param   {*} ...objs  The arrays or objects to merge
+ * @returns {*}          The merged arrays or objects
+ */
+function deepMerge (...objs) {
+
+	/**
+	 * Get the object type
+	 * @param  {*}       obj The object
+	 * @return {String}      The object type
+	 */
+	function getType (obj) {
+		return Object.prototype.toString.call(obj).slice(8, -1).toLowerCase();
+	}
+
+	/**
+	 * Deep merge two objects
+	 * @return {Object}
+	 */
+	function mergeObj (clone, obj) {
+		for (let [key, value] of Object.entries(obj)) {
+			let type = getType(value);
+			if (clone[key] !== undefined && getType(clone[key]) === type && ['array', 'object'].includes(type)) {
+				clone[key] = deepMerge(clone[key], value);
+			} else {
+				clone[key] = structuredClone(value);
+			}
+		}
+	}
+
+	// Create a clone of the first item in the objs array
+	let clone = structuredClone(objs.shift());
+
+	// Loop through each item
+	for (let obj of objs) {
+
+		// Get the object type
+		let type = getType(obj);
+
+		// If the current item isn't the same type as the clone, replace it
+		if (getType(clone) !== type) {
+			clone = structuredClone(obj);
+			continue;
+		}
+
+		// Otherwise, merge
+		if (type === 'array') {
+			clone = [...clone, ...structuredClone(obj)];
+		} else if (type === 'object') {
+			mergeObj(clone, obj);
+		} else {
+			clone = obj;
+		}
+
+	}
+
+	return clone;
 }
