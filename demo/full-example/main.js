ae3cae75930c12d615941bb42fce609874796f02
Loïc Cattani <loic@cattani.co>
2025-04-27 15:53:14 +0200

    Some examples projects

---
+++ demo/full-example/main.js
@@ -0,0 +1,75 @@
+import kaplay from "kaplay";
+import loquacePlugin from "/src/loquace.js";
+
+kaplay({
+  // styling
+  buttons: {
+    space: {
+      keyboard: ["space"],
+    },
+    z: {
+      keyboard: ["z"],
+    },
+  },
+  plugins: [loquacePlugin],
+});
+
+scene("main", () => {
+  loquace.init();
+
+  loadSprite("bean", "./bean.png");
+  loadSprite("skuller", "./skuller.png");
+
+  onButtonPress("space", () => {
+    if (loquace.config.showNextPrompt) loquace.next();
+  });
+
+  onButtonPress("z", () => {
+    loquace.config.pop.position = 'center';
+    loquace.start("finish");
+  });
+
+  loquace.registerCommand("sayHi", () => {
+    console.log("Hi!");
+    console.log("Press Z to finish the script");
+  });
+
+  loquace.characters({
+    r: {
+      name: "Robot",
+      expressions: {
+        happy: "bean",
+      },
+      defaultExpression: "happy",
+    },
+    t: {
+      name: "Tom",
+      expressions: {
+        happy: "skuller",
+      },
+      defaultExpression: "happy",
+      dialogType: "vn",
+      dialogOptions: {
+        dialogText: {
+          letterSpacing: 10,
+          color: RED,
+        },
+      },
+    },
+  });
+
+  loquace.script({
+    'begin': [
+      "r Hello, I am Bean.",
+      "t Hi, I am Skuller.",
+      "disableNextPrompt sayHi r I can say hi from the console too!",
+    ],
+    'finish': [
+      "r Loquace is awesome!",
+    ],
+  });
+
+  loquace.start("begin");
+});
+
+go("main");
